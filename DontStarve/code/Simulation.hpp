a297985e4fb26e0a9473bebbc27c8acca0dbc528
TiphaineROB <3672816@pc4044.polytech.upmc.fr>
2018-05-07 15:53:06 +0200

    Init commit

---
+++ DontStarve/code/Simulation.hpp
@@ -0,0 +1,24 @@
+/// ROB4-POO Projet Don't Starve
+/// Fichier : Simulation.hpp
+///	Autheurs : Benjamin Delbos et Tiphaine Diot
+/// Classe Simulation
+
+#ifndef SIM_HPP
+#define SIM_HPP
+
+#include <iostream>
+#include <Environnement.hpp>
+
+class Simulation{
+
+public: 
+
+
+protected:
+	double _timer;
+	Environnement _myEnv;
+	int _day;
+
+};
+
+#endif
