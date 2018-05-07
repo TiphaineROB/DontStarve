c7f736067996751d5b15d413867dd3588e8df011
TiphaineROB <3672816@pc4044.polytech.upmc.fr>
2018-05-07 16:23:15 +0200

    Ajout Fichiers

---
+++ code/Environnement.hpp
@@ -0,0 +1,21 @@
+/// ROB4-POO Projet Don't Starve
+/// Fichier : Environnement.hpp
+///	Autheurs : Benjamin Delbos et Tiphaine Diot
+/// Classe Environnement
+
+#ifndef ENV_HPP
+#define ENV_HPP
+
+#include <iostream>
+
+class Environnement{
+
+public: 
+
+
+protected:
+
+
+};
+
+#endif
